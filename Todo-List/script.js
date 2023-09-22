f42a5265aa9719955c33f5b39cd51be42dbd7de1
parth <113553406+parthu311@users.noreply.github.com>
2023-09-22 23:19:53 +0530

    Todo-list

---
+++ Todo-List/script.js
@@ -0,0 +1,36 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+
+ function addTask (){
+    if(inputBox.value === ''){
+        alert('You must Wirte something');
+    }else{
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveLocal()
+}   
+listContainer.addEventListener('click' , function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveLocal()
+
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveLocal()
+    }
+},false);
+
+function saveLocal(){
+    localStorage.setItem('data' , listContainer.innerHTML);
+}
+function showList(){
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+showList(); 
